c671126e8fa245d9f57aad27bf3b2d2c91a812a9
Sbusiso Mthimunye <48700283+SbusisoSGT@users.noreply.github.com>
2021-03-20 05:37:40 +0200

    Redesigned the coming soon page and added another one

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
-let tween = gsap.to("#model-image", {x: 60, duration: 1, opacity: 1, ease: "ease"});
+
+var countDownDate = new Date("Apr 19, 2021 09:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in countdown-container
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    window.location.href = "http://www.theoutcasts.co.za";
+  }
+}, 1000);
